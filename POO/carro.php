43423fa62d97613753ef1352659ab110f5c218e2
Prof Rafael <rafa.fernan10@gmail.com>
2020-07-13 14:49:56 -0300

    Corrigir erros

--- POO/carro.php
+++ POO/carro.php
@@ -1,28 +1,31 @@
-<?php 
-    class carro{		
-        public function getmarca(){
-            return $this -> marca; 
+<?php
+    class carro{		//nome da classe é sempre com inicial maiúscula. Corrigir todas as classes
+      //falta a criação dos atributos aqui
+
+      //os getters e setters tem que seguir o padrão camelCase.
+        public function getmarca(){ //getMarca, ao invés de getmarca
+            return $this -> marca;
         }
         public function setmarca($argmarca){
-             $this -> marca = $argmarca; 
+             $this -> marca = $argmarca;
         }
          public function getcor(){
-            return $this -> cor; 
+            return $this -> cor;
         }
         public function setcor($argcor){
-             $this -> cor = $argcor; 
+             $this -> cor = $argcor;
         }
          public function getplaca(){
-            return $this -> placa; 
+            return $this -> placa;
         }
         public function setplaca($argplaca){
-             $this -> placa = $argplaca; 
+             $this -> placa = $argplaca;
         }
          public function getmodelo(){
-            return $this -> modelo; 
+            return $this -> modelo;
         }
         public function setmodelo($argmodelo){
-             $this -> modelo = $argmodelo; 
+             $this -> modelo = $argmodelo;
         }
     }
 ?>
